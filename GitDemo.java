07efacaf1e4171ad7c2e6e015310662baba54816
Abraham Trinh <69036619+abrahamtrinh@users.noreply.github.com>
2021-02-26 13:02:55 -0700

    Update GitDemo.java
    Added my name to Line 2
    Modified addNumber() to properly return the inputted number and previous.
    Modified class to hold a new private double called reverseNum. When the reverseNumber() function is called, the returned value is assigned to reverseNum.

--- GitDemo.java
+++ GitDemo.java
@@ -1,9 +1,9 @@
 /*****
- * Edit this line to have your name.
+ * Abraham Trinh
  ******/
 public class GitDemo {
     
-    private double previousNumber;
+    private double previousNumber, reverseNum;
     
     public GitDemo() {
         previousNumber = 1;
@@ -14,7 +14,7 @@ public class GitDemo {
     *****/
     public double addNumber(double num) {
         System.out.print("Add num to previousNumber");
-        return 0;
+        return num + previousNumber;
     }
     
     /*****
@@ -45,7 +45,7 @@ public class GitDemo {
         for(int i = 1; i <= num; ++i){
             newNumber = newNumber * i;
         }
-        
+        reverseNum = newNumber;
         return newNumber;
     }
     
